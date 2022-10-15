7b61b614d60d05a93c2e3518f0077934b7c3c599
BSW <niv@cywareness.io>
2022-10-15 23:10:36 +0300

    added env

--- src/services/messagesData.js
+++ src/services/messagesData.js
@@ -1,27 +1,33 @@
-const baseUrl = 'http://localhost:5000';
+import { API_URL } from "./constants";
 
 export async function createChatRoom(receiver, message) {
-    return (await fetch(`${baseUrl}/messages/createChatRoom`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-        body: JSON.stringify({message: message, receiver: receiver})
-    })).json();
+  return (
+    await fetch(`${API_URL}/messages/createChatRoom`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ message: message, receiver: receiver }),
+    })
+  ).json();
 }
 
 export async function getUserConversations() {
-    return (await fetch(`${baseUrl}/messages/getUserConversations`, { credentials: 'include' })).json();
+  return (
+    await fetch(`${API_URL}/messages/getUserConversations`, { credentials: "include" })
+  ).json();
 }
 
 export async function sendMessage(chatId, message) {
-    return (await fetch(`${baseUrl}/messages/sendMessage`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-        body: JSON.stringify({chatId, message})
-    })).json();
+  return (
+    await fetch(`${API_URL}/messages/sendMessage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ chatId, message }),
+    })
+  ).json();
 }
