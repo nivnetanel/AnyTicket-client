71be02220ce155d83de13c64e12bf5d414ee46b1
lidan <lidandanino@h-MacBook-Pro-sl-lidan-2.local>
2023-01-03 14:53:56 +0200

    added a popup message

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,59 +6,6 @@ function Footer() {
     <footer>
       <div className="footer">
         <div className="row">
-          <ul>
-            <li>
-              <a href="#">
-                <i className="fa fa-facebook">
-                  <FaFacebookF />
-                </i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-instagram">
-                  <FaInstagram />
-                </i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-youtube">
-                  <FaYoutube />
-                </i>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-twitter">
-                  <FaTwitter />
-                </i>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="row">
-          <ul>
-            <li>
-              <a href="#">Contact us</a>
-            </li>
-            <li>
-              <a href="#">Our Services</a>
-            </li>
-            <li>
-              <a href="#">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="#">Terms & Conditions</a>
-            </li>
-            <li>
-              <a href="#">Career</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="row">
           ANYTICKET Copyright © 2022 AnyTicket - All rights reserved || Designed By: Niv&Lidan
         </div>
       </div>
